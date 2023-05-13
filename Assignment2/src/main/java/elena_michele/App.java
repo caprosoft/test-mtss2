5dca54f04e6be680231e698f8a63f84515107bd3
caprosoft <michele.caprotti@studenti.unipd.it>
2023-05-13 20:02:50 +0200

    creazione progetto maven

---
+++ Assignment2/src/main/java/elena_michele/App.java
@@ -0,0 +1,13 @@
+package elena_michele;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
