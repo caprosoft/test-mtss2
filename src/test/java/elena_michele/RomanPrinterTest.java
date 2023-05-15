07112318fb5366dc213a6d2eb2cda7836cb0b66f
caprosoft <michele.caprotti@studenti.unipd.it>
2023-05-15 23:03:50 +0200

    testa stampa singoli caratteri

--- src/test/java/elena_michele/RomanPrinterTest.java
+++ src/test/java/elena_michele/RomanPrinterTest.java
@@ -22,4 +22,89 @@ public class RomanPrinterTest {
         String output = RomanPrinter.print(1);    
         assertEquals(expectedOutput, output);
     }
+
+    @Test
+    public void testPrintV() {
+        String expectedOutput = "--      -- \n"+
+                                "\\ \\    / / \n"+
+                                " \\ \\  / /  \n"+
+                                "  \\ \\/ /   \n"+
+                                "   \\  /    \n"+
+                                "    \\/     \n";
+        
+        String output = RomanPrinter.print(5);    
+        assertEquals(expectedOutput, output);
+    }
+
+    @Test
+    public void testPrintX() {
+        String expectedOutput = " --    --  \n"+
+                                " \\ \\  / /  \n"+
+                                "  \\  V /   \n"+
+                                "   >   <   \n"+
+                                "  /  .  \\  \n"+
+                                " /__/ \\__\\ \n";
+        
+        String output = RomanPrinter.print(10);    
+        assertEquals(expectedOutput, output);
+    }
+
+    @Test
+    public void testPrintL() {
+        String expectedOutput = " -       \n"+
+                                "| |      \n"+
+                                "| |      \n"+
+                                "| |      \n"+
+                                "| |____  \n"+
+                                "|______| \n";
+        
+        String output = RomanPrinter.print(50);    
+        assertEquals(expectedOutput, output);
+    }
+
+    @Test
+    public void testPrintC() {
+        String expectedOutput = "  -----  \n"+
+                                " / ____| \n"+
+                                "| |      \n"+
+                                "| |      \n"+
+                                "| |____  \n"+
+                                " \\_____| \n";
+        
+        String output = RomanPrinter.print(100);    
+        assertEquals(expectedOutput, output);
+    }
+
+    @Test
+    public void testPrintD() {
+        String expectedOutput = " -----   \n"+
+                                "|  -- \\  \n"+
+                                "| |  | | \n"+
+                                "| |  | | \n"+
+                                "| |__| | \n"+
+                                "|_____/  \n";
+        
+        String output = RomanPrinter.print(500);    
+        assertEquals(expectedOutput, output);
+    }
+
+    @Test
+    public void testPrintM() {
+        String expectedOutput = " --  --  \n"+
+                                "|  \\/  | \n"+
+                                "| \\  / | \n"+
+                                "| |\\/| | \n"+
+                                "| |  | | \n"+
+                                "|_|  |_| \n"; 
+
+        
+        String output = RomanPrinter.print(1000);    
+        assertEquals(expectedOutput, output);
+    }
+
 }
+
+
+         
+        
+     
