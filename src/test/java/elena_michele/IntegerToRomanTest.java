c3e109059085099161700df4d23b050cf351b3d8
caprosoft <michele.caprotti@studenti.unipd.it>
2023-05-16 00:13:20 +0200

    creato primo test per convertitore

--- src/test/java/elena_michele/IntegerToRomanTest.java
+++ src/test/java/elena_michele/IntegerToRomanTest.java
@@ -4,3 +4,17 @@
 ////////////////////////////////////////////////////////////////////
 
 package elena_michele;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class IntegerToRomanTest {
+
+    @Test
+    public void testConvertI() {
+        int number = 1;
+        String expectedRoman = "I";
+        String actualRoman = IntegerToRoman.convert(number);
+        assertEquals(expectedRoman, actualRoman);
+    }
+}
