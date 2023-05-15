679af8cc3891da8aff034ee982236ab53682de13
caprosoft <michele.caprotti@studenti.unipd.it>
2023-05-15 19:27:45 +0200

    Inizio creazione test

--- src/test/java/elena_michele/IntegerToRomanTest.java
+++ src/test/java/elena_michele/IntegerToRomanTest.java
@@ -1,10 +1,24 @@
+////////////////////////////////////////////////////////////////////
+// ELENA CHILESE 2008074
+// MICHELE CAPROTTI 2009988
+////////////////////////////////////////////////////////////////////
+
 package elena_michele;
 
 import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.assertEquals;
 
 public class IntegerToRomanTest {
-    @Test
-    public void testConvert() {
 
+    private IntegerToRoman convert;
+    @Before
+    public void Before(){
+        convert = new IntegerToRoman();
     }
+    @Test
+    public void TestConvertOnetoRoman(){
+        assertEquals("I", convert.convert(1));
+  
+
 }
--- src/test/java/elena_michele/RomanPrinterTest.java
+++ src/test/java/elena_michele/RomanPrinterTest.java
@@ -1,10 +1,26 @@
+////////////////////////////////////////////////////////////////////
+// ELENA CHILESE 2008074
+// MICHELE CAPROTTI 2009988
+////////////////////////////////////////////////////////////////////
+
 package elena_michele;
 
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class RomanPrinterTest {
-    @Test
-    public void testPrint() {
 
+    @Test
+    public void testPrintI() {
+        String expectedOutput = " -----  \n"+
+                                "|_   _| \n"+
+                                "  | |   \n"+
+                                "  | |   \n"+
+                                " _| |_  \n"+
+                                "|_____| \n";
+        
+        String output = RomanPrinter.print(1);
+        
+        assertEquals(expectedOutput, output);
     }
 }
