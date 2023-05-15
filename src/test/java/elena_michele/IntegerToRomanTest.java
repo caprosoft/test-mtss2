503460e1bae741f0e7088741582e4da9d1f277a6
caprosoft <michele.caprotti@studenti.unipd.it>
2023-05-16 01:33:54 +0200

    espanso test convertitore

--- src/test/java/elena_michele/IntegerToRomanTest.java
+++ src/test/java/elena_michele/IntegerToRomanTest.java
@@ -16,5 +16,55 @@ public class IntegerToRomanTest {
         String expectedRoman = "I";
         String actualRoman = IntegerToRoman.convert(number);
         assertEquals(expectedRoman, actualRoman);
+    }
+
+    @Test
+    public void testConvertFiveToRoman() {
+        int number = 5;
+        String expectedRoman = "V";
+        String actualRoman = IntegerToRoman.convert(number);
+        assertEquals(expectedRoman, actualRoman);
     }   
+
+    @Test
+    public void testConvertTenToRoman() {
+        int number = 10;
+        String expectedRoman = "X";
+        String actualRoman = IntegerToRoman.convert(number);
+        assertEquals(expectedRoman, actualRoman);
+    }   
+
+    @Test
+    public void testConvertFiftyToRoman() {
+        int number = 50;
+        String expectedRoman = "L";
+        String actualRoman = IntegerToRoman.convert(number);
+        assertEquals(expectedRoman, actualRoman);
+    }   
+
+    @Test
+    public void testConvertOneHundredToRoman() {
+        int number = 100;
+        String expectedRoman = "C";
+        String actualRoman = IntegerToRoman.convert(number);
+        assertEquals(expectedRoman, actualRoman);
+    }   
+
+    @Test
+    public void testConvertFiveHundredToRoman() {
+        int number = 500;
+        String expectedRoman = "D";
+        String actualRoman = IntegerToRoman.convert(number);
+        assertEquals(expectedRoman, actualRoman);
+    }   
+
+    @Test
+    public void testConvertOneThousandToRoman() {
+        int number = 1000;
+        String expectedRoman = "M";
+        String actualRoman = IntegerToRoman.convert(number);
+        assertEquals(expectedRoman, actualRoman);
+    }
+    
+
 }
